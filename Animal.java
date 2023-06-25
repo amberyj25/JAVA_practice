ff0d5b38f86392e6b772e776923aa85c6868d74a
amberyj25 <amber20240@gmail.com>
2023-06-25 16:02:19 +0800

    practice JAVA

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public class Animal{
+  private String title;
+  private int age;
+  public Animal() {
+    this.title = "dog";
+    this.age = 5;
+  }
+  public String getTilte() {
+    return title;
+  }
+}
---
+++ Cool.java
@@ -0,0 +1,6 @@
+public class Cool{
+  public static void main(String[] args){
+    Animal dog = new Animal();
+    System.out.println(dog.getTilte());
+  }
+}
